1de77c0a6a1ffcebcd523d1dc43c3e2f34600248
Nie-Ying <274891993@qq.com>
2025-03-12 21:40:13 +0800

    基本完成“插入操作”的逻辑实现

--- A.insert.js
+++ A.insert.js
@@ -26,7 +26,7 @@ window.onload = function () {
         content.innerHTML = '';
         switch (target) {
             case 'insert':
-                window.location=("A.insert.html")
+                window.location = ("A.insert.html")
                 break;
             case 'update':
                 content.innerHTML = '<h2>修改记录</h2>';
@@ -44,4 +44,38 @@ window.onload = function () {
                 content.innerHTML = '<p>无效的操作</p>';
         }
     }
+
+    document.getElementById('submit',).addEventListener('click', function (event) {
+        event.preventDefault();
+        let data = {
+            index: document.getElementById('index').value,
+            semester: document.getElementById('semester').value,
+            studentNumber: document.getElementById('number').value,
+            name: document.getElementById('name').value,
+            class: document.getElementById('class').value,
+            tel: document.getElementById('tel').value,
+            course: document.getElementById('course').value,
+            score: document.getElementById('score').value,
+            teacher: document.getElementById('teacher').value
+        };
+        console.log(data)
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", "http://localhost:3111/api", true);
+        xhr.setRequestHeader("Content-Type", "application/json;charset=utf-8");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                const response = JSON.parse(xhr.responseText);
+                if (response.message === 'success') {
+                    alert('成功');
+                } else {
+                    alert('失败');
+                }
+            }
+        };
+        xhr.send(JSON.stringify({
+            target: "insert", data
+        }));
+    });
+
+
 }
